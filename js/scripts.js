56b1e433f60bc666bd6b20030abe87eac4452a42
Ivan <ivan.frontcoder@gmail.com>
2022-11-03 14:27:29 +0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -319,4 +319,61 @@ window.onload = function () {
   dropBlock($('.js-drop-btn'));
   dropBlock($('.js-drop-menu'), true);
 
+  // YandexMap
+  if ($('#contactMap').length) {
+    var point = $('#contactMap').data('point');
+    ymaps.ready(function () {
+      var myMap;
+      var geolocation = ymaps.geolocation,
+      myMap = new ymaps.Map("contactMap", {
+        center: point, // Центер карты
+        zoom: 15, // Коэффициент масштаба карты
+        controls: [ // Элементы управления
+          'zoomControl',
+          'geolocationControl'
+        ],
+      });
+      // Определить местоположение пользователя
+      geolocation.get({
+        provider: 'yandex',
+        mapStateAutoApply: true
+      }).then(function (result) {
+        // Красным цветом пометим положение, вычисленное через ip.
+        result.geoObjects.options.set('preset', 'islands#redCircleIcon');
+        result.geoObjects.get(0).properties.set({
+          balloonContentBody: 'Мое местоположение'
+        });
+        myMap.geoObjects.add(result.geoObjects);
+      });
+      // Добавление метки
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: 'Бани "Три богатыря"',
+      }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/mark-map.png',
+        iconImageSize: [30, 40],
+        iconImageOffset: [-15, -40]
+      }),
+      myMap.geoObjects
+        .add(myPlacemark);
+      // Дабавление маршрута
+    //   var multiRoute = new ymaps.multiRouter.MultiRoute({
+    //     // Описание опорных точек мультимаршрута.
+    //     referencePoints: [
+    //       point,
+    //       "г.Екатеринбург, ул. Норильская, 78"
+    //     ], 
+    //     // Параметры маршрутизации.
+    //     params: {
+    //       // Ограничение на максимальное количество маршрутов, возвращаемое маршрутизатором.
+    //       results: 2
+    //     }
+    //   }, {
+    //     // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
+    //     boundsAutoApply: true
+    //   });
+    //   myMap.geoObjects.add(multiRoute);
+    });
+  }
+
 }
